46100f0f2c3c3b50e9b042f4df0a5d5918cd310d
Kelley Muro <kelleymuro@gmail.com>
2019-10-01 19:44:58 -0700

    adding missing icons

--- src/component/Form.js
+++ src/component/Form.js
@@ -10,11 +10,11 @@ class Form extends Component {
                 <form className="formEl">
                     <input className="inputFields name" type="text" name="name" placeholder="Full Name"/>
                     <input className="inputFields address" type="text" name="address" placeholder="Address"/>
-                    <input className="inputFields side" type="text" name="zip" placeholder="Zip Code"/>
-                    <input className="inputFields side" type="text" name="state" placeholder="State"/>
+                    <input className="inputFields side zip" type="number" name="zip" placeholder="Zip Code"/>
+                    <input className="inputFields side state" type="text" name="state" placeholder="State"/>
                     <input className="inputFields phone" type="tel" name="phone1" placeholder="Cell Phone"/>
                     <input className="inputFields email" type="email" name="email" placeholder="Email Address" />
-                    <input className="inputFields" type="text" name="sqft" placeholder="Square Feet of Home"/>
+                    <input className="inputFields sqft" type="text" name="sqft" placeholder="Square Feet of Home"/>
                     <div className="btnContainer">
                         <p className="next">Next</p>
                         <button className="submitBtn" type="submit"><img alt="Arrow Button" src='https://denisealley.s3-us-west-2.amazonaws.com/Digital__Design_99-512.png'/></button>
